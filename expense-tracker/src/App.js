5e4220d5a0e82ff5d15b936f73e47f06c70096e8
aarushi353 <aarushiabrol999@gmail.com>
2021-07-04 16:10:06 +0530

    added form for new expenses

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,34 +1,34 @@
-import './App.css';
-import Expenses from './components/Expenses/Expenses';
+import "./App.css";
+import NewExpense from "./components/New Expense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 const App = () => {
   const expenses = [
     {
-      id: 'e1',
-      title: 'New book',
+      id: "e1",
+      title: "New book",
       amount: 99.31,
       date: new Date(2020, 10, 1),
     },
-    { id: 'e2', title: 'New TV', amount: 869.92, date: new Date(2021, 12, 17) },
+    { id: "e2", title: "New TV", amount: 869.92, date: new Date(2021, 12, 17) },
     {
-      id: 'e3',
-      title: 'New Chair',
+      id: "e3",
+      title: "New Chair",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: 'e4',
-      title: 'New Desk',
+      id: "e4",
+      title: "New Desk",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
   ];
   return (
     <div className="App">
-     <Expenses 
-     expenses={expenses}
-     />
+      <NewExpense />
+      <Expenses expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
